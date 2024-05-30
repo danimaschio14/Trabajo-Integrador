8e2dc87ceffb216c3951174de64e523c1b593834
SCenturionZ <sebastian.ignacio.centurion@gmail.com>
2024-05-30 08:08:30 -0300

    Update CRUD Activity

--- frontend/src/app/services/activity.service.ts
+++ frontend/src/app/services/activity.service.ts
@@ -13,10 +13,26 @@ getAllActivity():Observable<any> {
         // Realiza una solicitud HTTP GET para obtener todas las actividades
         return this.http.get(`${this.apiUrl}/activity`);
 }
+
+addActivity(activity: any):Promise<any>  {
+  return this.http.post(`${this.apiUrl}/activity`, activity).toPromise();
+}
+
+updateActivity(id: Number, activity: any):Promise<any>{
+  return this.http.patch(`${this.apiUrl}/activity/${id}`, activity).toPromise()
+}
+
 updateStatus(id: Number, value: string):Observable<any> {
   // Realiza una solicitud HTTP GET para obtener todas las actividades
   return this.http.patch(`${this.apiUrl}/activity/${id}`,{
     "status" : value
   });
 }
+
+getActivity(id:number):Observable<any>{
+  const url = `${this.apiUrl}/activity/${id}`;
+   // Realiza una solicitud HTTP GET para obtener una actividad
+  return this.http.get(url);
+}
+
 }
